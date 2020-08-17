9e6371a28717ca20da0e5348fd4ccdd05b7f4499
Peter McDermott <mcdermpete@gmail.com>
2020-08-17 13:48:08 +1200

    Got 3D run working

---
+++ Third-Person IK Controller/Assets/Scripts/Animation.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animation : MonoBehaviour
+{
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (animator == null) return;
+
+        var x = Input.GetAxis("Horizontal");
+        var y = Input.GetAxis("Vertical");
+
+        Move(x, y);
+    }
+
+    private void Move(float x, float y)
+    {
+        animator.SetFloat("VelX", x);
+        animator.SetFloat("VelY", y);
+
+    }
+        
+    
+}
